17aa8f9daab1c8b8e9ae5e5fbc7b6d390a68065e
sonoda796 <t.sonoda796@gmail.com>
2025-03-12 07:27:21 +0900

    [add]CI設定

---
+++ src/test/java/Test.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void testHelloWorld() {
+        String message = "Hello, World!";
+        assertEquals("Hello, World!", message);
+    }
+}
